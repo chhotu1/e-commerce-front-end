c9da918adf8ce3fae11625503458e4c328298de1
chhotu1 <chhotukumarsow@gmail.com>
2022-08-07 22:51:39 +0530

    add dashboard style

--- src/Pages/Admin/index.js
+++ src/Pages/Admin/index.js
@@ -4,6 +4,9 @@ import TopNav from '../../Components/admin-app/TopNav';
 import Notification from '../../Components/admin-app/dashboard/Notification';
 // import Sidebar from '../../Components/admin-app/Sidebar';
 // import Helper from './../../Helper'
+import {
+FaArrowRight,FaUserFriends,FaCalendarAlt,FaBirthdayCake
+} from 'react-icons/fa';
 const Admin = () => {
   const [toggled, setToggled] = useState(false);
   const handleToggleSidebar = (value) => {
@@ -17,8 +20,82 @@ const Admin = () => {
       />
       <div className='admin-content'>
         <TopNav handleToggleSidebar={handleToggleSidebar} />
-        <Notification/>
-        <p>welcome</p>
+        <Notification />
+
+        <div className='dashboard-content p-4'>
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-lg-3 col-12">
+                <div className="bg-primary small-box">
+                  <div className="inner">
+                    <h3>122</h3>
+                    <p>User</p>
+                  </div>
+                  <div className={"iconButton"}>
+                    <FaUserFriends />
+                  </div>
+                  <div className="icon">
+                    <i className="ion ion-bag"></i>
+                  </div>
+                  <a className={"small_box_footer"}>More info
+                    <FaArrowRight />
+                  </a>
+                </div>
+              </div>
+              <div className="col-lg-3 col-12">
+                <div className="bg-success small-box">
+                  <div className="inner">
+                    <h3>12</h3>
+                    <p>Leave</p>
+                  </div>
+                  <div className={"iconButton"}>
+                    <FaUserFriends />
+                  </div>
+                  <div className="icon">
+                    <i className="ion ion-bag"></i>
+                  </div>
+                  <a className={"small_box_footer"}>More info
+                    <FaArrowRight />
+                  </a>
+                </div>
+              </div>
+              <div className="col-lg-3 col-12">
+                <div className="bg-danger small-box">
+                  <div className="inner">
+                    <h3>122</h3>
+                    <p>Holiday</p>
+                  </div>
+                  <div className={"iconButton"}>
+                    <FaCalendarAlt />
+                  </div>
+                  <div className="icon">
+                    <i className="ion ion-bag"></i>
+                  </div>
+                  <a className={"small_box_footer"}>More info
+                    <FaArrowRight />
+                  </a>
+                </div>
+              </div>
+              <div className="col-lg-3 col-12">
+                <div className="bg-info small-box">
+                  <div className="inner">
+                    <h3>122</h3>
+                    <p>Birthday wises</p>
+                  </div>
+                  <div className={"iconButton"}>
+                    <FaBirthdayCake />
+                  </div>
+                  <div className="icon">
+                    <i className="ion ion-bag"></i>
+                  </div>
+                  <a className={"small_box_footer"}>More info
+                    <FaArrowRight />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
