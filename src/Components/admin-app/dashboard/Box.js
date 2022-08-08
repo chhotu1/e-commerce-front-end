3e62981bf4d0060888882a7e1abdb4dd78d5c953
chhotu1 <chhotukumarsow@gmail.com>
2022-08-08 06:15:12 +0530

    dashboard

---
+++ src/Components/admin-app/dashboard/Box.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import {
+    FaArrowCircleRight, FaUserFriends, FaCalendarAlt, FaBirthdayCake
+} from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import Helper from '../../../Helper';
+const Box = () => {
+    const DashboardMenu = [
+        { id: 2, count: 20, title: 'Leave', url: Helper.RouteName.LEAVE.MAIN, color: 'info', icon: <FaUserFriends /> },
+        { id: 3, count: 120, title: 'Holidays', url: Helper.RouteName.ADMIN, color: 'success', icon: <FaCalendarAlt /> },
+        { id: 4, count: 10, title: 'Birthday wises', url: Helper.RouteName.ADMIN, color: 'danger', icon: <FaBirthdayCake /> },
+    ];
+    return (
+        <div className='dashboard-content p-4'>
+            <div className="container-fluid">
+                <div className="row">
+                    {DashboardMenu.map((item, index) => {
+                        return (
+                            <div className="col-lg-3 col-12" key={index}>
+                                <div className={`bg-${item.color} small-box`}>
+                                    <div className="inner">
+                                        <h3>{item.count}</h3>
+                                        <p>{item.title}</p>
+                                    </div>
+                                    <div className={"iconButton"}>
+                                        {item.icon}
+                                    </div>
+                                    <div className="icon">
+                                        <i className="ion ion-bag"></i>
+                                    </div>
+                                    <Link className="small_box_footer" to={item.url}>
+                                    More info
+                                        <FaArrowCircleRight />
+                                    </Link>
+                                   
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Box
--- src/Pages/Admin/index.js
+++ src/Pages/Admin/index.js
@@ -4,9 +4,8 @@ import TopNav from '../../Components/admin-app/TopNav';
 import Notification from '../../Components/admin-app/dashboard/Notification';
 // import Sidebar from '../../Components/admin-app/Sidebar';
 // import Helper from './../../Helper'
-import {
-FaArrowRight,FaUserFriends,FaCalendarAlt,FaBirthdayCake
-} from 'react-icons/fa';
+
+import Box from '../../Components/admin-app/dashboard/Box';
 const Admin = () => {
   const [toggled, setToggled] = useState(false);
   const handleToggleSidebar = (value) => {
@@ -21,81 +20,7 @@ const Admin = () => {
       <div className='admin-content'>
         <TopNav handleToggleSidebar={handleToggleSidebar} />
         <Notification />
-
-        <div className='dashboard-content p-4'>
-          <div className="container-fluid">
-            <div className="row">
-              <div className="col-lg-3 col-12">
-                <div className="bg-primary small-box">
-                  <div className="inner">
-                    <h3>122</h3>
-                    <p>User</p>
-                  </div>
-                  <div className={"iconButton"}>
-                    <FaUserFriends />
-                  </div>
-                  <div className="icon">
-                    <i className="ion ion-bag"></i>
-                  </div>
-                  <a className={"small_box_footer"}>More info
-                    <FaArrowRight />
-                  </a>
-                </div>
-              </div>
-              <div className="col-lg-3 col-12">
-                <div className="bg-success small-box">
-                  <div className="inner">
-                    <h3>12</h3>
-                    <p>Leave</p>
-                  </div>
-                  <div className={"iconButton"}>
-                    <FaUserFriends />
-                  </div>
-                  <div className="icon">
-                    <i className="ion ion-bag"></i>
-                  </div>
-                  <a className={"small_box_footer"}>More info
-                    <FaArrowRight />
-                  </a>
-                </div>
-              </div>
-              <div className="col-lg-3 col-12">
-                <div className="bg-danger small-box">
-                  <div className="inner">
-                    <h3>122</h3>
-                    <p>Holiday</p>
-                  </div>
-                  <div className={"iconButton"}>
-                    <FaCalendarAlt />
-                  </div>
-                  <div className="icon">
-                    <i className="ion ion-bag"></i>
-                  </div>
-                  <a className={"small_box_footer"}>More info
-                    <FaArrowRight />
-                  </a>
-                </div>
-              </div>
-              <div className="col-lg-3 col-12">
-                <div className="bg-info small-box">
-                  <div className="inner">
-                    <h3>122</h3>
-                    <p>Birthday wises</p>
-                  </div>
-                  <div className={"iconButton"}>
-                    <FaBirthdayCake />
-                  </div>
-                  <div className="icon">
-                    <i className="ion ion-bag"></i>
-                  </div>
-                  <a className={"small_box_footer"}>More info
-                    <FaArrowRight />
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Box/>
       </div>
     </div>
   );
